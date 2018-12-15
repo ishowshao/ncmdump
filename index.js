be2966c1316f30a12a5909eaf650aceb3c2071f5
邵帅 <ishowshao@me.com>
2018-12-16 00:24:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,12 +23,9 @@ const coreKey = new Uint8Array([0x68, 0x7A, 0x48, 0x52, 0x41, 0x6D, 0x73, 0x6F, 
 
 const aesEcb = new aes.ModeOfOperation.ecb(coreKey);
 
-const decodedKeyData = aesEcb.decrypt(keyData);
+const decodedKeyData = aes.padding.pkcs7.strip(aesEcb.decrypt(keyData));
 
-// for (let i = 0; i < decodedKeyData.length; i++) {
-//     console.log(decodedKeyData[i].toString(2))
-// }
-const trimKeyData = decodedKeyData.slice(17, decodedKeyData.indexOf(15));
+const trimKeyData = decodedKeyData.slice(17);
 console.log(1, Buffer.from(decodedKeyData).toString('ascii'));
 
 const metaLength = file.readUInt32LE(globalOffset);
@@ -45,8 +42,12 @@ const base64decode = Buffer.from(Buffer.from(metaData.slice(22)).toString('ascii
 
 const metaKey = new Uint8Array([0x23, 0x31, 0x34, 0x6C, 0x6A, 0x6B, 0x5F, 0x21, 0x5C, 0x5D, 0x26, 0x30, 0x55, 0x3C, 0x27, 0x28]);
 const aseMeta = new aes.ModeOfOperation.ecb(metaKey);
-const meatArray = aseMeta.decrypt(base64decode);
-console.log(Buffer.from(meatArray).toString('ascii'));
+const meatArray = aes.padding.pkcs7.strip(aseMeta.decrypt(base64decode));
+
+const metaJson = Buffer.from(meatArray).toString('utf8');
+console.log(metaJson);
+const metaObject = JSON.parse(metaJson.substr(6));
+console.log(metaObject);
 
 // read crc32 check
 file.readUInt32LE(globalOffset);
